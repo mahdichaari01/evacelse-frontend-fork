bf84d6386c81bfbe390600ce4497769f52080b04
mahdichaari01 <92325467+mahdichaari01@users.noreply.github.com>
2022-12-04 22:13:43 +0100

    configured daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,29 @@ module.exports = {
 		extend: {},
 	},
 	plugins: [require("daisyui")],
+	daisyui: {
+		styled: true,
+		themes: [
+			{
+				light: {
+					primary: "#009D94",
+					secondary: "#373737",
+					accent: "#6CD276",
+					neutral: "",
+					"base-100": "#FFFFFF",
+					"base-200": "#F5F5F7",
+					"base-300": "#EBEBEB",
+					error: "#DC4504",
+					warning: "#D3A500",
+					success: "009D94",
+				},
+			},
+			
+		],
+		base: true,
+		utils: true,
+		logs: true,
+		rtl: false,
+		prefix: "",
+	},
 };
