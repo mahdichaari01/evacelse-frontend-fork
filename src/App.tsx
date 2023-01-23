008b5e154b7e86c985f9594f08648e22df9a6e4f
mahdichaari01 <92325467+mahdichaari01@users.noreply.github.com>
2023-01-23 20:53:16 +0100

    testinghusky

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-					import React from "react";
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -9,7 +9,7 @@ function App() {
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
 					Edit <code>src/App.tsx</code> and save to reload.
-									</p>
+				</p>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
