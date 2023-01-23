6d6e70bfb375ba558b71ded6cf464293033dc1e0
mahdichaari01 <92325467+mahdichaari01@users.noreply.github.com>
2023-01-23 22:26:27 +0100

    added storybook with absolute paths and tailwind support

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { a } from "@/test/a";
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>
+				<p className="bg-islamic-green">
 					Edit <code>src/App.tsx</code> and save to reload.
 				</p>
 				<a
