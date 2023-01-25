a4fb2ba34b3e4f5536287488613f11e99d8cb2e2
mahdichaari01 <92325467+mahdichaari01@users.noreply.github.com>
2023-01-25 15:13:04 +0100

    added basic app setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { AppProvider } from "./providers/app";
+import { AppRoutes } from "./routes";
 
 function App() {
-	return <></>;
+	return (
+		<AppProvider>
+			<AppRoutes />
+		</AppProvider>
+	);
 }
 
 export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from "react-router-dom";
+
+export const AppRoutes = () => {
+	return (
+		<Routes>
+			<Route path="/" element={<></>} />
+		</Routes>
+	);
+};
