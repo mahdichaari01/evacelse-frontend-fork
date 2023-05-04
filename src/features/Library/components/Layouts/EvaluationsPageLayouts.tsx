975a825e00075c57ba99f3bca4992604156b514f
mahdichaari01 <92325467+mahdichaari01@users.noreply.github.com>
2023-05-04 10:50:15 +0100

    improved ux by ajusting sizing

--- src/features/Library/components/Layouts/EvaluationsPageLayouts.tsx
+++ src/features/Library/components/Layouts/EvaluationsPageLayouts.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export const EvaluationPageLayout = (props: PropsWithChildren) => {
 	return (
-		<div className="w-full max-md:h-[calc(100vh-5.1rem)] h-full overflow-hidden max-md:px-3 max-md:pb-2 grid grid-cols-3 grid-rows-[1fr,3.3rem] gap-2 lg:grid-cols-[20.375rem,1fr] lg:grid-rows-2 lg:gap-5 2xl:grid-cols-[20.25rem,1fr,20.25rem] 2xl:grid-rows-1">
+		<div className="w-full max-md:h-[calc(100vh-5.1rem)] h-full overflow-hidden max-md:px-3 max-md:pb-2 grid grid-cols-3 grid-rows-[1fr,3.3rem] gap-2 lg:grid-cols-[20.375rem,1fr] lg:grid-rows-2 lg:gap-5 xl:grid-cols-[20.25rem,1fr,20.25rem] xl:grid-rows-1">
 			{props.children}
 		</div>
 	);
@@ -15,13 +15,13 @@ export const QuestionContainer = (props: PropsWithChildren) => (
 );
 
 export const EvalsListContainer = (props: PropsWithChildren) => (
-	<div className="col-start-1 row-start-2 row-end-3 lg:row-start-1 lg:row-end-2 2xl:row-end-3">
+	<div className="col-start-1 row-start-2 row-end-3 lg:row-start-1 lg:row-end-2 xl:row-end-3">
 		{props.children}
 	</div>
 );
 
 export const QuestionListContainer = (props: PropsWithChildren) => (
-	<div className="col-start-2 col-end-4 row-start-2 row-end-3 lg:row-start-2 lg:row-end-3 lg:col-start-1 lg:col-end-2 2xl:row-start-1 2xl:row-end-3 2xl:col-start-3 2xl:col-end-4	">
+	<div className="col-start-2 col-end-4 row-start-2 row-end-3 lg:row-start-2 lg:row-end-3 lg:col-start-1 lg:col-end-2 xl:row-start-1 xl:row-end-3 xl:col-start-3 xl:col-end-4	">
 		{props.children}
 	</div>
 );
